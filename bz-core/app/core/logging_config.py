430b4787df1851d67de7e1a196d666ff59244376
tsavaliya-myb <tsavaliya.myb@gmail.com>
2026-05-17 14:21:02 +0530

    logger issue fix

--- bz-core/app/core/logging_config.py
+++ bz-core/app/core/logging_config.py
@@ -15,7 +15,6 @@ def configure_logging() -> None:
     structlog.configure(
         processors=[
             structlog.stdlib.add_log_level,           # injects "level" key
-            structlog.stdlib.add_logger_name,         # injects "logger" key
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
